be18fee6d84883cf3c49a52a2711cb2bafbf9d94
hernanofx <hernandorra@gmail.com>
2025-07-24 14:28:21 -0300

    webhook 2

--- backend/routes/webhook.js
+++ backend/routes/webhook.js
@@ -1,6 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
+// Endpoint GET para ver estado desde navegador
+router.get('/', (req, res) => {
+  res.status(200).send('Webhook activo y esperando mensajes POST.');
+});
+
 // Endpoint para recibir webhooks de Yoitzen
 router.post('/', async (req, res) => {
   console.log('Webhook recibido:', req.body);
